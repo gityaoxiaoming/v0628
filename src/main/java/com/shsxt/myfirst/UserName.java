0a73444bc1c3751eab6ffb2dab95ab86f8bb8229
yao <123456@qq.com>
2018-06-28 20:17:28 +0800

    yes

---
+++ src/main/java/com/shsxt/myfirst/UserName.java
@@ -0,0 +1,10 @@
+package com.shsxt.myfirst;
+
+/**
+ * Created by yao on 2018/6/28.
+ */
+public class UserName {
+    public static void main(String[] args) {
+        System.out.println("aaaaa");
+    }
+}
