a334a46b9a58a7bb1805e7962f39cd859f7b8bbd
yao <123456@qq.com>
2018-06-28 16:06:59 +0800

    第一个版本

---
+++ src/com/shsxt/dao/UserDao.java
@@ -0,0 +1,10 @@
+package com.shsxt.dao;
+
+/**
+ * Created by yao on 2018/6/28.
+ */
+public class UserDao {
+    public static void main(String[] args) {
+        System.out.println("hello word");
+    }
+}
